767dfd78e86817b1822d769528b7658507eb2729
Picantino31 <Aaroncheng1611@gmail.com>
2024-12-03 15:24:37 -0700

    a

--- src/Assignments/As2_Team.java
+++ src/Assignments/As2_Team.java
@@ -4,5 +4,17 @@ public class As2_Team {
     private String nicknames;
     private String city;
     private String league;
+    private double money;
+    private int TotalWins;
+    private int TotalLost;
+
+    public As2_Team(String n, String c, String le, double m, int w, int l){
+        nicknames = n;
+        city = c;
+        league = le;
+        money = m;
+        TotalWins = w;
+        TotalLost = l;
+    }
     
 }
